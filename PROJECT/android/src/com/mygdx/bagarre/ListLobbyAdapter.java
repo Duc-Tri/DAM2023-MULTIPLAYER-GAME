8573cbefb3847a09a2e7a4ab4ec8aae461ef0c2e
Narloulou <123882067+Narloulou@users.noreply.github.com>
2023-03-20 17:33:28 +0100

    Interface Lobby (reorganisation de l'ecran game mode a faire)

---
+++ PROJECT/android/src/com/mygdx/bagarre/ListLobbyAdapter.java
@@ -0,0 +1,65 @@
+package com.mygdx.bagarre;
+
+import android.annotation.SuppressLint;
+import android.content.Context;
+import android.content.Intent;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+public class ListLobbyAdapter extends RecyclerView.Adapter<ListLobbyAdapter.MyViewHolder> {
+
+    Context context;
+    int[] numLobby;
+
+    public ListLobbyAdapter() {
+    }
+
+    public ListLobbyAdapter(Context context,int[] numLobby ) {
+        this.context = context;
+        this.numLobby = numLobby;
+
+    }
+
+    @NonNull
+    @Override
+    public MyViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        LayoutInflater layoutInflater = LayoutInflater.from(context);
+        View view = layoutInflater.inflate(R.layout.item_list_lobby,parent,false);
+
+        return new MyViewHolder(view);
+    }
+
+    @SuppressLint("SetTextI18n")
+    @Override
+    public void onBindViewHolder(@NonNull MyViewHolder holder, int position) {
+        holder.tvNomLobby.setText("Lobby n° "+numLobby[position]);
+
+
+    }
+
+
+
+    @Override
+    public int getItemCount() {
+        return numLobby.length;
+    }
+
+    public class MyViewHolder extends RecyclerView.ViewHolder{
+
+        private TextView tvNomLobby;
+        private Button btnJoinLobby;
+
+        public MyViewHolder(@NonNull View itemView) {
+            super(itemView);
+            tvNomLobby = itemView.findViewById(R.id.tvNomLobby);
+            btnJoinLobby = itemView.findViewById(R.id.btnJoinLobby);
+        }
+    }
+}
